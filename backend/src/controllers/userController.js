b329013f4a2058cd31e18b2f9ca897b9ccb95d46
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2025-01-14 21:00:54 +0700

    2. API Register and Hash Password

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,12 @@
+const { createCustomerService } = require("../services/userService");
+
+const createUser = async (req, res) => {
+    const { name, email, password } = req.body
+    const data = await createCustomerService(name, email, password)
+    return res.status(200).json(data);
+}
+
+
+module.exports = {
+    createUser,
+}
